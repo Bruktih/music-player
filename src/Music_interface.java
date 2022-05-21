2be51c28b65bcc8dc4c48a837bee9ecc6247b2c8
bruktih <bruktihailu43@gmail.com>
2022-05-21 05:06:27 +0200

    first push

---
+++ src/Music_interface.java
@@ -0,0 +1,23 @@
+
+import jaco.mp3.player.MP3Player;
+
+import java.io.File;
+import javax.swing.JFileChooser;
+
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import pirates.music.PiratesMusic;
+
+
+
+   
+
+public static class piratesEndless{
+   
+
+   
+    public static void main(String args[]) {
+       System.out.println("Hello, World!"); 
+    }
+
+}
